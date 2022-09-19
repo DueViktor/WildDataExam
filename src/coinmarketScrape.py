feb53c3633c6884605b5ffb7a925d55a03578622
DueViktor <viktorduep@gmail.com>
2022-09-19 11:27:19 +0200

    corrected the bitcoin

--- src/coinmarketScrape.py
+++ src/coinmarketScrape.py
@@ -12,7 +12,7 @@ def scrape_it(coin_name:str):
     df.to_csv(f'data/coinmarket-{coin_name}.csv')
 
 if __name__ == '__main__':
-    coins = ['Bitcoin','XRP','ETC','SOL','DOT'] 
+    coins = ['BTC','XRP','ETC','SOL','DOT'] 
     for coin in coins:
         print(f'Scraping {coin}')
         scrape_it(coin_name=coin)
