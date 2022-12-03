3a0f6b2a2ae25b56c102ba842c745c112ea5b537
Askenm <aske.osv@gmail.com>
2022-12-03 11:33:28 +0100

    pushing rolling zscore

--- CreateCorrPlots.py
+++ CreateCorrPlots.py
@@ -3,16 +3,12 @@ import matplotlib.pyplot as plt
 import dtale
 import numpy as np
 
-threshold = 0.8
-
-
+threshold = 0.7
 
 
 dataset_raw = pd.read_csv('data/final_datasets/aggregated_dataset.tsv',sep=';')
+print(dataset_raw.sort_values('date').head())
 dataset = dataset_raw[['date','High','Low','Mean','event_id','event_type','period_type']].drop_duplicates()
-
-
-
 
 
 dims = ['conflict','similarity','knowledge','power','status','support','identity','romance','trust','fun']
@@ -20,26 +16,38 @@ dims = ['conflict','similarity','knowledge','power','status','support','identity
 
 corr_dfs = {}
 for et in dataset['event_type'].unique():
-    if isinstance(et,float):
-        continue
-    
-    dataset_onehot = dataset_raw.loc[(dataset_raw['event_type']==et)]
 
+    if isinstance(et,float):
+        et = 'NoEvent'
+        dataset_onehot = dataset_raw.loc[(dataset_raw['event_type'].isna())]
+    else:
+        dataset_onehot = dataset_raw.loc[(dataset_raw['event_type']==et)]
+    print(dataset_onehot.shape)
+    
     for dim in dims:
         dataset_onehot[dim] = dataset_onehot[dim].apply(lambda x:1  if x>threshold else 0)
 
-
-    cols = ['date']+dims
-
+    cols = ['date']+dims+['Mean']
+    dataset_onehot = dataset_onehot[cols].groupby('date').sum().sort_index()
+    dataset_out = dataset_onehot.rolling(6,center=True).mean().dropna().sort_index()
+    for dim in dims:
+        mu = dataset_out[dim].mean()
+        std = dataset_out[dim].std()
+        if std:
+            dataset_out[dim] = dataset_out[dim].apply(lambda x: (x-mu)/std)
+            
+        else:
+            dataset_out.drop(dim,axis=1,inplace=True)
+            print('Dropping',dim)
     ## Z SCORE
-
     if et not in corr_dfs:
-        corr_dfs[et] = dataset_onehot[cols]
+        corr_dfs[et] = dataset_out.sort_index()
     else:
-        corr_dfs[et] = pd.concat([corr_dfs[et],dataset_onehot[cols]])
+        corr_dfs[et] = pd.concat([corr_dfs[et],dataset_out])
             
 
 
-print(corr_dfs['positive'].shape)
-print(corr_dfs['negative'].shape)
-print(corr_dfs['positive'].head())
+for k,v in corr_dfs.items():
+    v.reset_index().to_csv('data/final_datasets/{}_zscores.csv'.format(k),index=None,sep=';')
+
+
